97b6a17f5730d7693e4e2e63c94dbe1560dec061
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-10-17 12:17:48 -0500

    Module 5 lab ready

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+require('datejs');
+//Creating a combined user function
+function combineUsers(...args) {
+//arg is an array with arrays (for usernames)
+// creating an object  to hold the final result
+const combineObject = {
+  users: []
+};
+// creating a loop through each array inside args
+for (let array of args) {
+  // arrays here later
+  // merging the arrays into combinedObject.users
+  combineObject.users.push(...array);
+}
+// adding today's date using DateJS
+combineObject.merge_date = new Date().toString("M/d/yyyy");
+//return object
+return combineObject;
+}
+
 
 
 
